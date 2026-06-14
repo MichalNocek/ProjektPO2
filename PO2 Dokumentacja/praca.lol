53336ae7dc9c32d75654a39ec757138ebb871c83
Michał Nocek <michaln057@gmail.com>
2026-06-14 19:21:11 +0200

    Poprawy

---
+++ PO2 Dokumentacja/praca.lol
@@ -0,0 +1,4 @@
+\contentsline {lstlisting}{\numberline {4.1}{\ignorespaces Struktura katalogów projektu.}}{12}{lstlisting.4.1}%
+\contentsline {lstlisting}{\numberline {7.1}{\ignorespaces Haszowanie hasła i logowanie.}}{20}{lstlisting.7.1}%
+\contentsline {lstlisting}{\numberline {7.2}{\ignorespaces Bezpieczne usuwanie testu (zachowanie integralności).}}{20}{lstlisting.7.2}%
+\contentsline {lstlisting}{\numberline {7.3}{\ignorespaces Ocenianie pojedynczego pytania (GradingService).}}{21}{lstlisting.7.3}%
--- ProjektPO2/obj/Debug/net10.0-windows/ProjektPO2.AssemblyInfo.cs
+++ ProjektPO2/obj/Debug/net10.0-windows/ProjektPO2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjektPO2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e69f18767341268c3fc5d501a309f7003d355d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjektPO2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjektPO2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
